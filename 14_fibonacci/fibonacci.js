751ebe78f44060021810b12a5632110c0eccf16b
elifnurozkaya <elifnurozkaya4334@gmail.com>
2025-08-09 16:16:32 +0300

    fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = +num;
 
+    if(num == 0)
+        return 0;
+    else if(num<0)
+        return "OOPS";
+    
+    let prev = 0;
+    let curr = 1;
+    let next;
+
+    for(let i=1;i<num;i++){
+        next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+    return curr;
 };
 
 // Do not edit below this line
