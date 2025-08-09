7004102965d6235b363b8c0e8773d0a971dcb1f5
elifnurozkaya <elifnurozkaya4334@gmail.com>
2025-08-09 17:05:48 +0300

    object practices

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const now = new Date();
+let deathYear ;
+let age;
+let oldestAge;
+const findTheOldest = function(people) {
+    let oldest = people[0];
+    people.forEach((person)=> {
+        deathYear = person.yearOfDeath || now.getFullYear();
 
+        age = deathYear - person.yearOfBirth; 
+        oldestAge = (oldest.yearOfDeath || now.getFullYear()) - oldest.yearOfBirth;
+        if(age > oldestAge)
+            oldest = person;
+    });
+    return oldest;
+        
 };
 
 // Do not edit below this line
